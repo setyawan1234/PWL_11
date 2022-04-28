c4dbb220ac522687559e12f70be533946fd438ff
setyawan1234 <twentyzee1@gmail.com>
2022-04-28 11:36:52 +0700

    ubah isi function authorize di TodoRequest

--- app/Http/Requests/TodoRequest.php
+++ app/Http/Requests/TodoRequest.php
@@ -14,7 +14,10 @@ class TodoRequest extends ApiRequest
      */
     public function authorize()
     {
-        return false;
+        if ($this->method() == Request::METHOD_POST)
+            return true;
+        $todo = $this->route('todo');
+        return auth()->user()->id == $todo->user_id;
     }
 
     /**
